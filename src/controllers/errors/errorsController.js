a1059d707c23952746d7e1d774b2e39addf81f24
Kennedy Nyaga <kennedy.nyaga@qlik.com>
2020-10-09 06:48:20 +0200

    feat: handle error in prod vs dev

--- src/controllers/errors/errorsController.js
+++ src/controllers/errors/errorsController.js
@@ -1,10 +1,43 @@
+const sendErrorToDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stackTrace: err.stack,
+  });
+};
+
+const sendErrorToProd = (err, res) => {
+  // Trusted error: ie operational errors
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Unknown errors
+    //Log using logger..Here we will use console instead
+    console.error(`Error 💥: ${err}`);
+
+    //Send generic error
+    res.status(500).json({
+      status: err.status,
+      message: 'Whoooops something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorToDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorToProd(err, res);
+  }
+
   next();
 };
